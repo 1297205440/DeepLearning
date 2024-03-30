55fa16ba93e811983337fc3f96371947732f358f
1297205440 <165153681+1297205440@users.noreply.github.com>
2024-03-30 18:28:18 +0800

    Delete src directory

--- src/ResNet/predict.py
+++
@@ -1,9 +0,0 @@
-import atexit
-import subprocess
-
-def run_next_program():
-    subprocess.run(['python', 'train.py'])  # 第二个函数是下一个程序的位置
-
-if __name__ == '__main__':
-    print(True)
-    atexit.register(run_next_program)
